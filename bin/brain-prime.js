642d6e27f1050ecbb9978ded234f07847ef9b456
Amakoff <amakoffn@gmail.com>
2023-07-30 21:39:36 +0400

    Changed the line

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,17 +3,6 @@ import readlineSync from 'readline-sync';
 
 
 const brainPrime = () => {
-	const isPrime = (num) => {
-		if (num <= 1) {
-			return false;
-		}
-		for (let i = 2; i < Math.sqrt(num); i += 1){
-			if (num % i === 0) {
-				return false;
-			}
-		}
-		return true;
-	}
 	
 	console.log('Welcome to the Brain Games!');
 	const name = readlineSync.question('May I have your name? ');
@@ -22,15 +11,29 @@ const brainPrime = () => {
 
 	let count = 0;
 	while (count < 3) {
+		
 		const number = Math.floor(Math.random() * 100) + 1;
+		
+		const numberPrime = (number) => {
+                if (number <= 1) {
+                        return false;
+                }
+                for (let i = 2; i < Math.sqrt(number); i += 1){
+                        if (number % i === 0) {
+                                return false;
+                        }
+                }
+                return true;
+	}
+		const isPrime = numberPrime(number);
 		console.log(`Question: ${number}`);
 		const userAnswer = readlineSync.question('Your answer: ');
 		
-		if((isPrime(number) &&  userAnswer === 'yes') || (!isPrime(number) && answer === 'no')){
+		if((numberPrime(number) &&  userAnswer === 'yes') || (!numberPrime(number) && userAnswer === 'no')){
 			console.log('Correct!');
 			count++;
 		}else{
-			console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isPrime ? 'yes' : 'no'}'.`};
+			console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isPrime ? 'yes' : 'no'}'.`);
 			console.log(`Let's try again, ${name}!`);
 			return;
 		}
