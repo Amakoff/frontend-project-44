7979c3f26eb143bbcd2f8f4e8cf54c3a03f95f41
Amakoff <amakoffn@gmail.com>
2023-07-30 22:03:46 +0400

    Linter Edit

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,29 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-export default function greeting(){
- console.log('Welcome to the Brain Games!');
- const name = readlineSync.question('May I have your name? ');
 
- console.log(`Hello, ${name}!`);
- console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
- const getRandomNum = (max) => {
-	return Math.floor(Math.random() * max); 
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const getRandomNum = (max) => {
+    return Math.floor(Math.random() * max);
 };
- for (let i = 0; i < 3; i += 1){
-
-	const number = getRandomNum(100);
-	console.log(`Question: ${number}`);
-	const answerUser = readlineSync.question('Your answer: ');
-
-	const check = (number % 2 === 0) && (answerUser === 'yes');
-	const check2 = (number % 2 !== 0) && (answerUser === 'no');
-
-	if(check === true || check2 === true) {
-		console.log('Correct!');
-	}else {
-		const opposite = (answerUser === 'yes' ? 'no' : 'yes');
-		const errorMessage = console.log(`"${answerUser}" is wrong answer ;(. Correct answer was '${opposite}'. \nLet's try again, ${name}!`);
-		return errorMessage;
-	  }
-	}
-	console.log(`Congratulations, ${name}!`);
+  for (let i = 0; i < 3; i += 1){
+    const number = getRandomNum(100);
+    console.log(`Question: ${number}`);
+    const answerUser = readlineSync.question('Your answer: ');
+    const check = (number % 2 === 0) && (answerUser === 'yes');
+    const check2 = (number % 2 !== 0) && (answerUser === 'no');
+    if(check === true || check2 === true) {
+      console.log('Correct!');
+    }else {
+      const opposite = (answerUser === 'yes' ? 'no' : 'yes');
+      const errorMessage = console.log(`"${answerUser}" is wrong answer ;(. Correct answer was '${opposite}'. \nLet's try again, ${name}!`);
+      return errorMessage;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 };
 
 greeting();
