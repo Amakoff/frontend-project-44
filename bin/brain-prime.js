6a45619aba5a2b3bb0cf3e0d9b613ad1dfcff1fe
Amakoff <amakoffn@gmail.com>
2023-08-06 20:48:07 +0400

    changed the line

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -2,43 +2,42 @@
 import readlineSync from 'readline-sync';
 
 
-const brainPrime = () => {
-	
-	console.log('Welcome to the Brain Games!');
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
-	console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-	let count = 0;
-	while (count < 3) {
-		
-		const number = Math.floor(Math.random() * 100) + 1;
-		
-		const numberPrime = (number) => {
-                if (number <= 1) {
-                        return false;
-                }
-                for (let i = 2; i < Math.sqrt(number); i += 1){
-                        if (number % i === 0) {
-                                return false;
-                        }
-                }
-                return true;
-	}
-		const isPrime = numberPrime(number);
-		console.log(`Question: ${number}`);
-		const userAnswer = readlineSync.question('Your answer: ');
-		
-		if((numberPrime(number) &&  userAnswer === 'yes') || (!numberPrime(number) && userAnswer === 'no')){
-			console.log('Correct!');
-			count++;
-		}else{
-			console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isPrime ? 'yes' : 'no'}'.`);
-			console.log(`Let's try again, ${name}!`);
-			return;
-		}
-	}
-	console.log(`Congratulations, ${name}!`);
-};
-
-brainPrime();
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+function playPrimeGame() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(
+    'Answer "yes" if given number is prime. Otherwise answer "no".'
+  );
+  let correctAnswersCount = 0;
+  while (correctAnswersCount < 3) {
+    const number = Math.floor(Math.random() * 100);
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer.toLowerCase() === 'yes' && isPrime(number)) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else if (userAnswer.toLowerCase() === 'no' && !isPrime(number)) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      const correctAnswer = isPrime(number) ? 'yes' : 'no';
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+}
+playPrimeGame();
