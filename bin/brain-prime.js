18e7d34ce8cc21e382587dbebe82fcd13d615d01
Amakoff <amakoffn@gmail.com>
2023-08-12 21:41:33 +0400

    add new str

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -5,7 +5,7 @@ function isPrime(number) {
   if (number < 2) {
     return false;
   }
-  for (let i = 2; i <= Math.sqrt(number); i++) {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
       return false;
     }
